bb54385ed8cb46c910e9aa500d91601f1cd72b5f
CGsama <cgsama@outlook.com>
2024-01-25 11:55:45 -0500

    cascading local interaction marker on threads

--- packages/backend/src/misc/set-local-interaction.ts
+++ packages/backend/src/misc/set-local-interaction.ts
@@ -1,11 +1,19 @@
 import { Notes, DriveFiles } from "@/models/index.js";
 
 export async function setLocalInteraction( noteid: string ) {
-	const note = await await Notes.findOneBy({ id: noteid });
+	const note = await Notes.findOneBy({ id: noteid });
 	
 	await Notes.update(noteid, {localInteraction: true});
 
 	for (const fileid of note.fileIds) {
 		await DriveFiles.update(fileid, {localInteraction: true});
+	}
+
+	if(note.replyId != null){
+		setLocalInteraction(note.replyId);
+	}
+
+	if(note.renoteId != null){
+		setLocalInteraction(note.replyId);
 	}
 }
